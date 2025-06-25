6dbb2d2512a045ad5662b05d250a184dee89238b
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2025-06-25 19:09:31 +0200

    adding env variable for backend example

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,14 @@ import { TodoForm } from '../components/TodoForm';
 import { TodoList } from '../components/TodoList';
 import { Filter } from '../components/Filter';
 
+
+const API_URL = import.meta.env.VITE_BACKEND_URL;
+
 export function Home() {
     return (
         <div className="page">
             <h1>Todo List</h1>
+            <p>API URL: {API_URL}</p>
             <TodoForm />
             <Filter />
             <TodoList />
